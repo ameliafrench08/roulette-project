06f2e7a6b7f33f8909da18e6b6c239a964348641
Amelia French <126724144+ameliafrench08@users.noreply.github.com>
2025-06-03 10:44:31 -0400

    Add files via upload

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -35,122 +35,127 @@ func _process(delta):
 			#if round(angular_speed) == 1:
 				#change = 0.001
 			
-		var angle_in_degrees = round(rad_to_deg(get_angle_to(Vector2(600, 0))))
+		var angle_in_degrees = rad_to_deg(get_angle_to(Vector2(600, 0)))
+		
 		
 		if angle_in_degrees < 0:
 			angle_in_degrees += 360
+		print(angle_in_degrees)
 		
 		if round(angular_speed) == 0:
 			moving = false
-			if angle_in_degrees >= 0 && angle_in_degrees <= 9.729:
+			print("----------------------------")
+			print(angle_in_degrees)
+			print("----------------------------")
+			if angle_in_degrees >= 355.135135135 && angle_in_degrees <= 4.86486485797:
 				angle = 34
 			
-			if angle_in_degrees >= 9.729 && angle_in_degrees <= 19.458:
+			if angle_in_degrees >= 4.86486485797 && angle_in_degrees <= 14.5945945877:
 				angle = 6
 			
-			if angle_in_degrees >= 19.458 && angle_in_degrees <= 29.187:
+			if angle_in_degrees >= 14.5945945877 && angle_in_degrees <= 24.3243243174:
 				angle = 27
 			
-			if angle_in_degrees >= 29.187 && angle_in_degrees <= 38.916:
+			if angle_in_degrees >= 24.3243243174 && angle_in_degrees <= 34.0540540471:
 				angle = 13
 			
-			if angle_in_degrees >= 38.916 && angle_in_degrees <= 48.645:
+			if angle_in_degrees >= 34.0540540471 && angle_in_degrees <= 43.7837837768:
 				angle = 36
 				
-			if angle_in_degrees >= 48.645 && angle_in_degrees <= 58.374:
+			if angle_in_degrees >= 43.7837837768 && angle_in_degrees <= 53.5135135065:
 				angle = 11
 				
-			if angle_in_degrees >= 58.374 && angle_in_degrees <= 68.103:
+			if angle_in_degrees >= 53.5135135065 && angle_in_degrees <= 63.2432432362:
 				angle = 30
 			
-			if angle_in_degrees >= 68.103 && angle_in_degrees <= 77.832:
+			if angle_in_degrees >= 63.2432432362 && angle_in_degrees <= 72.9729729659:
 				angle = 8
 			
-			if angle_in_degrees >= 77.832 && angle_in_degrees <= 87.561:
+			if angle_in_degrees >= 72.9729729659 && angle_in_degrees <= 82.7027026956:
 				angle = 23
 				
-			if angle_in_degrees >= 87.561 && angle_in_degrees <= 97.29:
+			if angle_in_degrees >= 82.7027026956 && angle_in_degrees <= 92.4324324253:
 				angle = 10
 			
-			if angle_in_degrees >= 97.29 && angle_in_degrees <= 107.019:
+			if angle_in_degrees >= 92.4324324253 && angle_in_degrees <= 102.162162155:
 				angle = 5
 				
-			if angle_in_degrees >= 107.019 && angle_in_degrees <= 116.748:
+			if angle_in_degrees >= 102.162162155 && angle_in_degrees <= 111.891891885:
 				angle = 24
 				
-			if angle_in_degrees >= 116.748 && angle_in_degrees <= 126.477:
+			if angle_in_degrees >= 111.891891885 && angle_in_degrees <= 121.621621615:
 				angle = 33
 			
-			if angle_in_degrees >= 126.477 && angle_in_degrees <= 136.206:
+			if angle_in_degrees >= 121.621621615 && angle_in_degrees <= 131.351351345:
 				angle = 1
 				
-			if angle_in_degrees >= 136.206 && angle_in_degrees <= 145.935:
+			if angle_in_degrees >= 131.351351345 && angle_in_degrees <= 141.081081075:
 				angle = 20
 				
-			if angle_in_degrees >= 145.935 && angle_in_degrees <= 155.664:
+			if angle_in_degrees >= 141.081081075 && angle_in_degrees <= 150.810810805:
 				angle = 14
 				
-			if angle_in_degrees >= 155.664 && angle_in_degrees <= 165.393:
+			if angle_in_degrees >= 150.810810805 && angle_in_degrees <= 160.540540535:
 				angle = 31
 				
-			if angle_in_degrees >= 165.393 && angle_in_degrees <= 175.122:
+			if angle_in_degrees >= 160.540540535 && angle_in_degrees <= 170.270270265:
 				angle = 16
 				
-			if angle_in_degrees >= 175.122 && angle_in_degrees <= 184.851:
+			if angle_in_degrees >= 170.270270265 && angle_in_degrees <= 179.999999995:
 				angle = 9
 			
-			if angle_in_degrees >= 184.851 && angle_in_degrees <= 194.58:
+			if angle_in_degrees >= 179.999999995 && angle_in_degrees <= 189.729729725:
 				angle = 22
 				
-			if angle_in_degrees >= 194.58 && angle_in_degrees <= 204.309:
+			if angle_in_degrees >= 189.729729725 && angle_in_degrees <= 199.459459455:
 				angle = 18
 			
-			if angle_in_degrees >= 204.309 && angle_in_degrees <= 214.038:
+			if angle_in_degrees >= 199.459459455 && angle_in_degrees <= 209.189189185:
 				angle = 29
 				
-			if angle_in_degrees >= 214.038 && angle_in_degrees <= 223.767:
+			if angle_in_degrees >= 209.189189185 && angle_in_degrees <= 218.918918915:
 				angle = 7
 			
-			if angle_in_degrees >= 223.767 && angle_in_degrees <= 233.496:
+			if angle_in_degrees >= 218.918918915 && angle_in_degrees <= 228.648648645:
 				angle = 28
 			
-			if angle_in_degrees >= 233.496 && angle_in_degrees <= 243.255:
+			if angle_in_degrees >= 228.648648645 && angle_in_degrees <= 238.378378375:
 				angle = 12
 			
-			if (angle_in_degrees) >= 243.255 && (angle_in_degrees) <= 252.954:
+			if (angle_in_degrees) >= 238.378378375 && (angle_in_degrees) <= 248.108108105:
 				angle = 35
 				
-			if (angle_in_degrees) >= 252.954 && (angle_in_degrees) <= 262.683:
+			if (angle_in_degrees) >= 248.108108105 && (angle_in_degrees) <= 257.837837835:
 				angle = 3
 				
-			if (angle_in_degrees) >= 262.683 && (angle_in_degrees) <= 272.412:
+			if (angle_in_degrees) >= 257.837837835 && (angle_in_degrees) <= 267.567567565:
 				angle = 26
 			
-			if (angle_in_degrees) >= 272.412 && (angle_in_degrees) <= 282.141:
+			if (angle_in_degrees) >= 267.567567565 && (angle_in_degrees) <= 277.297297295:
 				angle = 0
 			
-			if (angle_in_degrees) >= 282.141 && (angle_in_degrees) <= 291.87:
+			if (angle_in_degrees) >= 277.297297295 && (angle_in_degrees) <= 287.027027025:
 				angle = 32
 			
-			if (angle_in_degrees) >= 291.87 && (angle_in_degrees) <= 301.599:
+			if (angle_in_degrees) >= 287.027027025 && (angle_in_degrees) <= 296.756756755:
 				angle = 15
 			
-			if (angle_in_degrees) >= 301.599 && (angle_in_degrees) <= 311.328:
+			if (angle_in_degrees) >= 296.756756755 && (angle_in_degrees) <= 306.486486485:
 				angle = 19
 			
-			if (angle_in_degrees) >= 311.328 && (angle_in_degrees) <= 321.057:
+			if (angle_in_degrees) >= 306.486486485 && (angle_in_degrees) <= 316.216216215:
 				angle = 4
 			
-			if (angle_in_degrees) >= 321.057 && (angle_in_degrees) <= 330.786:
+			if (angle_in_degrees) >= 316.216216215 && (angle_in_degrees) <= 325.945945945:
 				angle = 21
 				
-			if angle_in_degrees >= 330.786 && (angle_in_degrees) <= 340.515:
+			if angle_in_degrees >= 325.945945945 && (angle_in_degrees) <= 335.675675675:
 				angle = 2
 			
-			if angle_in_degrees >= 340.515 && angle_in_degrees <= 350.244:
+			if angle_in_degrees >= 335.675675675 && angle_in_degrees <= 345.405405405:
 				angle = 25
 			
-			if angle_in_degrees >= 350.244:
+			if angle_in_degrees >= 345.405405405:
 				angle = 17
 			spun = true
 			moving = false
